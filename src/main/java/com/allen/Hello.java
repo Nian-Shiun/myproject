93b4ed098ab3ecfdcbd5f8854f0fe2c52b932b5a
Nian-Shiun <cool26876303@gmail.com>
2021-07-04 16:30:20 +0800

    Java and Kotlin variable, date type

--- src/main/java/com/allen/Hello.java
+++ src/main/java/com/allen/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //    System.out.println("Hello world");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        double Height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Allen";
     }
 }
