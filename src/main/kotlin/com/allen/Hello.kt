495728660e17d8f93a1e997bdf1ec2287aa45b91
Nian-Shiun <cool26876303@gmail.com>
2021-07-04 16:00:30 +0800

    Initial commit

---
+++ src/main/kotlin/com/allen/Hello.kt
@@ -0,0 +1,15 @@
+package com.allen
+
+fun main() {
+//    print("Hello kotlin")
+//    Human().hello()
+    val h =Human()
+    h.hello()
+}
+
+
+class Human {
+    fun  hello(){
+        println("Hello Kotlin")
+    }
+}
